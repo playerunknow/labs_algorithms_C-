2583ad011b00ae4c17c8bb1906eb461df6d6ecea
User <fghfhg@>
2024-09-23 13:19:20 +0300

    -

--- labs_algorithms_C++/labs_algorithms_C++.cpp
+++ labs_algorithms_C++/labs_algorithms_C++.cpp
@@ -1,80 +1,68 @@
 ﻿#include <iostream>
 #include <vector>
-#include <algorithm>  // для std::swap
-#include <cstdlib>    // для std::rand і std::srand
-#include <ctime>      // для std::time
-#include <chrono>     // для вимірювання часу
+#include <algorithm>  // для swap
+#include <ctime>      // для вимірювання часу
+#include <random>     // для генерації випадкових чисел
 
 using namespace std;
-using namespace std::chrono;
 
 // Функція для вимірювання часу виконання
-template <typename Func, typename... Args>
-void timeit(Func func, const string& name, Args&&... args) 
-{
-    auto start = high_resolution_clock::now();
-    func(forward<Args>(args)...);
-    auto end = high_resolution_clock::now();
-    duration<double> execution_time = end - start;
-    cout << "execution_time " << name << ": " << execution_time.count() << " seconds\n\n";
-}
+#define TIMEIT(func) \
+    { \
+        clock_t start_time = clock(); \
+        func; \
+        clock_t end_time = clock(); \
+        double execution_time = double(end_time - start_time) / CLOCKS_PER_SEC; \
+        cout << "execution_time " #func ": " << execution_time << " секунд\n\n"; \
+    }
 
 // Функція для генерації випадкового масиву
-vector<int> generate_random_array(int n, int lower_bound = 0, int upper_bound = 100) 
-{
-    vector<int> arr(n);
-    for (int i = 0; i < n; ++i) 
-    {
-        arr[i] = lower_bound + rand() % (upper_bound - lower_bound + 1);
+vector<int> generate_random_array(int n, int lower_bound = 0, int upper_bound = 100) {
+    vector<int> array(n);
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(lower_bound, upper_bound);
+
+    for (int i = 0; i < n; ++i) {
+        array[i] = dis(gen);
     }
-    return arr;
+    return array;
 }
 
-// BubbleSort
-void BubbleSort(vector<int>& arr) 
-{
+// Сортування бульбашкою (BubbleSort)
+void BubbleSort(vector<int>& arr) {
     int n = arr.size();
-    for (int i = 0; i < n - 1; ++i) 
-    {
-        for (int j = 0; j < n - i - 1; ++j) 
-        {
-            if (arr[j] > arr[j + 1]) 
-            {
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);
             }
         }
     }
 }
 
-// InsertionSort
+// Сортування вставками (InsertionSort)
 void InsertionSort(vector<int>& arr) {
     int n = arr.size();
-    for (int i = 1; i < n; ++i) 
-    {
+    for (int i = 1; i < n; ++i) {
         int current_value = arr[i];
         int j = i - 1;
-        while (j >= 0 && arr[j] > current_value) 
-        {
+        while (j >= 0 && arr[j] > current_value) {
             arr[j + 1] = arr[j];
-            j--;
+            --j;
         }
         arr[j + 1] = current_value;
     }
 }
 
-// QuickSort
-vector<int> quicksort(const vector<int>& arr) 
-{
-    if (arr.size() <= 1) 
-    {
-        return arr;
-    }
+// Швидке сортування (QuickSort)
+vector<int> quicksort(const vector<int>& arr) {
+    if (arr.size() <= 1) return arr;
 
     int pivot = arr[arr.size() / 2];
     vector<int> left, middle, right;
 
-    for (int x : arr) 
-    {
+    for (int x : arr) {
         if (x < pivot) left.push_back(x);
         else if (x == pivot) middle.push_back(x);
         else right.push_back(x);
@@ -83,80 +71,72 @@ vector<int> quicksort(const vector<int>& arr)
     left = quicksort(left);
     right = quicksort(right);
 
-    vector<int> result;
-    result.insert(result.end(), left.begin(), left.end());
-    result.insert(result.end(), middle.begin(), middle.end());
-    result.insert(result.end(), right.begin(), right.end());
-
-    return result;
+    left.insert(left.end(), middle.begin(), middle.end());
+    left.insert(left.end(), right.begin(), right.end());
+    return left;
 }
 
-int partition(vector<int>& arr, int low, int high) 
-{
+int partition(vector<int>& arr, int low, int high) {
     int pivot = arr[(low + high) / 2];
     int i = low - 1;
     int j = high + 1;
 
-    while (true)
-    {
-        do 
-        {
-            i++;
+    while (true) {
+        do {
+            ++i;
         } while (arr[i] < pivot);
 
-        do 
-        {
-            j--;
+        do {
+            --j;
         } while (arr[j] > pivot);
 
-        if (i >= j) 
-        {
-            return j;
-        }
+        if (i >= j) return j;
 
         swap(arr[i], arr[j]);
     }
 }
 
-
-// Hoare's QuickSort
-void quicksortHoare(vector<int>& arr, int low, int high) 
-{
-    if (low < high) 
-    {
+// Швидке сортування Хоара (Hoare's QuickSort)
+void quicksortHoare(vector<int>& arr, int low, int high) {
+    if (low < high) {
         int pi = partition(arr, low, high);
         quicksortHoare(arr, low, pi);
         quicksortHoare(arr, pi + 1, high);
     }
 }
 
-
-int main() 
-{
-    srand(time(0));  // Ініціалізація генератора випадкових чисел
-
-    vector<int> random_array = generate_random_array(10000, 0, 10000);
+// Функція для виконання сортувань та вимірювання часу
+void calculation(const vector<int>& random_array) {
     vector<int> main_array = random_array;
 
-    timeit(BubbleSort, "BubbleSort", main_array);
-    main_array = random_array;
-
-    timeit(InsertionSort, "InsertionSort", main_array);
-    main_array = random_array;
-
-    auto start = high_resolution_clock::now();
-    vector<int> sorted_array = quicksort(main_array);
-    auto end = high_resolution_clock::now();
-    duration<double> execution_time = end - start;
-    cout << "execution_time quicksort: " << execution_time.count() << " seconds\n\n";
+    TIMEIT(BubbleSort(main_array));
 
     main_array = random_array;
+    TIMEIT(InsertionSort(main_array));
 
-    start = high_resolution_clock::now();
-    quicksortHoare(main_array, 0, main_array.size() - 1);
-    end = high_resolution_clock::now();
-    execution_time = end - start;
-    cout << "execution_time quicksortHoare: " << execution_time.count() << " seconds\n\n";
+    main_array = random_array;
+    TIMEIT(quicksort(main_array));
+
+    main_array = random_array;
+    TIMEIT(quicksortHoare(main_array, 0, main_array.size() - 1));
+
+    cout << "\n\n\n\n\n\n\n";
+}
+
+int main() {
+    vector<int> random_array1 = generate_random_array(1000, 0, 10000);
+    vector<int> random_array2 = generate_random_array(2000, 0, 10000);
+    vector<int> random_array3 = generate_random_array(3000, 0, 10000);
+    vector<int> random_array4 = generate_random_array(4000, 0, 10000);
+    vector<int> random_array5 = generate_random_array(5000, 0, 10000);
+    vector<int> random_array6 = generate_random_array(6000, 0, 10000);
+
+    calculation(random_array1);
+    calculation(random_array2);
+    calculation(random_array3);
+    calculation(random_array4);
+    calculation(random_array5);
+    calculation(random_array6);
 
     return 0;
 }
